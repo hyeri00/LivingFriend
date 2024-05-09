4717a1007113b2420ba4e1fc4c72bb5b674d8ab3
hyeri00 <nexusring@gmail.com>
2024-05-10 02:30:12 +0900

    initial commit

---
+++ LivingFriend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LivingFriend
+//
+//  Created by 혜리 on 5/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
