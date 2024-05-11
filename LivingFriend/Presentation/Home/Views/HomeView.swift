de015af8e2b6c3b211f01d1e004d00955c4eed75
hyeri00 <nexusring@gmail.com>
2024-05-11 23:41:52 +0900

    feat: calendar UI 추가

--- LivingFriend/Presentation/Home/Views/HomeView.swift
+++ LivingFriend/Presentation/Home/Views/HomeView.swift
@@ -6,8 +6,27 @@
 //
 
 import UIKit
+import FSCalendar
+import SnapKit
 
 final class HomeView: UIView {
     
+    private lazy var calendar: FSCalendar = {
+        let calendar = FSCalendar(frame: .zero)
+        calendar.backgroundColor = .white
+        
+        calendar.locale = Locale(identifier: "ko_KR")
+        return calendar
+    }()
     
+    // MARK: - Initialize
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
