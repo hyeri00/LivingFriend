8a5432a552e6491faaae3a95a7b625f970e980f3
hyeri00 <nexusring@gmail.com>
2024-05-13 23:25:33 +0900

    feat: Typograhpy 정의

---
+++ LivingFriend/DesignSystem/Values/Typography.swift
@@ -0,0 +1,32 @@
+//
+//  Typography.swift
+//  LivingFriend
+//
+//  Created by 혜리 on 5/13/24.
+//
+
+import UIKit
+
+enum Font: String {
+    case bold = "Cafe24Ssurround"
+    case air = "Cafe24SsurroundAir"
+    
+    struct Typography {
+        
+        static func setFont(font: Font, fontSize: CGFloat) -> UIFont? {
+            return UIFont(name: font.rawValue, size: fontSize)
+        }
+        
+        static let bold18 = setFont(font: .bold, fontSize: 18)
+        static let air18 = setFont(font: .air, fontSize: 18)
+        
+        static let bold14 = setFont(font: .bold, fontSize: 14)
+        static let air14 = setFont(font: .air, fontSize: 14)
+    
+        static let bold12 = setFont(font: .bold, fontSize: 12)
+        static let air12 = setFont(font: .air, fontSize: 12)
+        
+        static let bold10 = setFont(font: .bold, fontSize: 10)
+        static let air10 = setFont(font: .air, fontSize: 10)
+    }
+}
