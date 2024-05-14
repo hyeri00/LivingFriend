a3b798489e3b69d07258099d21c90c34d35145d7
hyeri00 <nexusring@gmail.com>
2024-05-15 02:19:36 +0900

    feat: tabelView UI 추가

--- LivingFriend/Presentation/Category/Views/CategoryView.swift
+++ LivingFriend/Presentation/Category/Views/CategoryView.swift
@@ -5,4 +5,22 @@
 //  Created by 혜리 on 5/14/24.
 //
 
-import Foundation
+import UIKit
+
+final class CategoryView: UIView {
+    
+    // MARK: - UI
+    
+    private lazy var listCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.backgroundColor = .white
+        
+        view.register(CategoryCollectionViewCell.self,
+                      forCellWithReuseIdentifier: "CategoryCollectionViewCell")
+        return view
+    }()
+}
