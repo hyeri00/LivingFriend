2e8091e5b9a69c1cf663188d6f89ebbe38794885
hyeri00 <nexusring@gmail.com>
2024-05-15 02:28:58 +0900

    feat: text 분리 작업하는 매니저 생성

---
+++ LivingFriend/DesignSystem/Values/TextManager.swift
@@ -0,0 +1,17 @@
+//
+//  TextManager.swift
+//  LivingFriend
+//
+//  Created by 혜리 on 5/15/24.
+//
+
+import Foundation
+
+struct TextManager {
+    
+    static let category_appliance = "정수기 필터"
+    static let category_basin = "세면대 필터"
+    static let category_shower = "샤워기 필터"
+    static let category_washDishes = "수세미 교체"
+    static let category_washingMachine = "세탁조 클리너"
+}
