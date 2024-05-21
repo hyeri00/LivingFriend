3b2a71775c6a91729e13c45366204838b9b70268
hyeri00 <nexusring@gmail.com>
2024-05-21 22:22:30 +0900

    feat: 내비게이션바 설정

--- LivingFriend/Presentation/Category/PeriodViewController.swift
+++ LivingFriend/Presentation/Category/PeriodViewController.swift
@@ -20,4 +20,46 @@ final class PeriodViewController: UIViewController {
         
         self.view = periodView
     }
+    
+    // MARK: - Initialize
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        
+        self.setNavigationBar()
+    }
+    
+    private func setNavigationBar() {
+        let image = IconImages.back.image?.withTintColor(.black, renderingMode: .alwaysOriginal)
+        
+        let titleLabel = UILabel()
+        titleLabel.text = TextManager.period_title
+        titleLabel.font = Font.Typography.bold16
+        titleLabel.textColor = .black
+        titleLabel.sizeToFit()
+        self.navigationItem.titleView = titleLabel
+        
+        self.navigationController?.navigationBar.barTintColor = .white
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: image, style: .done,
+            target: self, action: #selector(showPrevious)
+        )
+    }
+    
+    @objc
+    private func showPrevious() {
+        self.dismiss(animated: true)
+    }
 }
