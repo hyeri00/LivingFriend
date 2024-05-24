68bb884f133127fba67cbeb57aab9d956a363a87
hyeri00 <nexusring@gmail.com>
2024-05-24 23:04:22 +0900

    feat: text 분리 처리

--- LivingFriend/DesignSystem/Values/TextManager.swift
+++ LivingFriend/DesignSystem/Values/TextManager.swift
@@ -14,6 +14,7 @@ struct TextManager {
     static let category_shower = "샤워기 필터"
     static let category_washDishes = "수세미 교체"
     static let category_washingMachine = "세탁조 클리너"
+    static let category_sink = "주방 수전 필터"
     
     static let manage_title = "관리 항목 추가"
     static let period_title = "주기 설정"
