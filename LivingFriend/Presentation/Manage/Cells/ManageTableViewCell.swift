69a6ae53b00bd6e5a8d15c126922bcd6f2129d46
hyeri00 <nexusring@gmail.com>
2024-05-26 05:41:50 +0900

    feat: layout 추가

--- LivingFriend/Presentation/Manage/Cells/ManageTableViewCell.swift
+++ LivingFriend/Presentation/Manage/Cells/ManageTableViewCell.swift
@@ -9,6 +9,12 @@ import UIKit
 
 final class ManageTableViewCell: UITableViewCell {
     
+    // MARK: - Metric
+    
+    private enum Metric {
+        static let basePadding: CGFloat = 20
+    }
+
     // MARK: - UI
     
     private let titleLabel: UILabel = {
@@ -31,4 +37,46 @@ final class ManageTableViewCell: UITableViewCell {
         label.font = Font.Typography.air14
         return label
     }()
+    
+    // MARK: - Initialize
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        self.backgroundColor = .white
+        self.selectionStyle = .none
+        
+        self.makeConstraints()
+    }
+    
+    private func makeConstraints() {
+        self.addSubview(self.titleLabel)
+        self.addSubview(self.manageLabel)
+        self.addSubview(self.dateLabel)
+        
+        self.titleLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(Metric.basePadding)
+            $0.centerY.equalToSuperview()
+        }
+        
+        self.manageLabel.snp.makeConstraints {
+            $0.trailing.equalTo(self.dateLabel.snp.leading).offset(-Metric.basePadding)
+            $0.centerY.equalToSuperview()
+        }
+        
+        self.dateLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview().offset(-Metric.basePadding)
+            $0.centerY.equalToSuperview()
+        }
+    }
 }
