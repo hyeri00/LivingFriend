0e7e42b7b76b1c002f395d6a9ff4acbf7cd91ec3
hyeri00 <nexusring@gmail.com>
2024-05-29 19:13:55 +0700

    feat: delegate/dataSource 채택

--- LivingFriend/Presentation/Manage/Views/ManageView.swift
+++ LivingFriend/Presentation/Manage/Views/ManageView.swift
@@ -11,14 +11,36 @@ final class ManageView: UIView {
     
     // MARK: - UI
     
-    private let manageTableView: UITableView = {
+    private lazy var manageTableView: UITableView = {
         let view = UITableView()
         view.rowHeight = 52
         view.backgroundColor = .white
         view.separatorStyle = .none
         
+        view.delegate = self
+        view.dataSource = self
         view.register(ManageTableViewCell.self,
                       forCellReuseIdentifier: "ManageTableViewCell")
         return view
     }()
+}
+
+extension ManageView: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return 1
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ManageTableViewCell",
+                                                 for: indexPath) as! ManageTableViewCell
+        
+        return cell
+    }
 }
