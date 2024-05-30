8df5c54017117feda54a78c8bc22ae128fb088bd
hyeri00 <nexusring@gmail.com>
2024-05-30 10:24:44 +0700

    feat: backgroundColor 지정

--- LivingFriend/Presentation/Manage/Views/ManageView.swift
+++ LivingFriend/Presentation/Manage/Views/ManageView.swift
@@ -23,6 +23,25 @@ final class ManageView: UIView {
                       forCellReuseIdentifier: "ManageTableViewCell")
         return view
     }()
+    
+    // MARK: - Initialize
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        self.backgroundColor = .white
+    }
+    
 }
 
 extension ManageView: UITableViewDelegate, UITableViewDataSource {
