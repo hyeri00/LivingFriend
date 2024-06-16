cb65f967fe4d24d04ef3dd3379c4ac3ba048f722
hyeri00 <nexusring@gmail.com>
2024-06-16 21:25:26 +0900

    feat: TableViewCell layout 추가

--- LivingFriend/Presentation/Home/Cells/HomeTableViewCell.swift
+++ LivingFriend/Presentation/Home/Cells/HomeTableViewCell.swift
@@ -9,6 +9,15 @@ import UIKit
 
 final class HomeTableViewCell: UITableViewCell {
     
+    // MARK: - Metric
+    
+    private enum Metric {
+        static let titleTopMargin: CGFloat = 10
+        static let leadingMargin: CGFloat = 24
+        static let periodTopSpacing: CGFloat = 10
+        static let periodBottomMargin: CGFloat = 5
+    }
+    
     // MARK: - UI
     
     private let titleLabel: UILabel = {
@@ -31,4 +40,48 @@ final class HomeTableViewCell: UITableViewCell {
         label.font = Font.Typography.air10
         return label
     }()
+    
+    // MARK: - Initialize
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        self.backgroundColor = .white
+        self.selectionStyle = .blue
+        
+        self.makeConstraints()
+    }
+    
+    private func makeConstraints() {
+        self.addSubview(self.titleLabel)
+        self.addSubview(self.periodLabel)
+        self.addSubview(self.dateLabel)
+        
+        self.titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(Metric.titleTopMargin)
+            $0.leading.equalToSuperview().offset(Metric.leadingMargin)
+        }
+        
+        self.periodLabel.snp.makeConstraints {
+            $0.top.equalTo(self.titleLabel.snp.bottom).offset(Metric.periodTopSpacing)
+            $0.leading.equalTo(self.titleLabel.snp.leading)
+            $0.bottom.equalToSuperview().offset(-Metric.periodBottomMargin)
+        }
+        
+        self.dateLabel.snp.makeConstraints {
+            $0.top.equalTo(self.periodLabel.snp.top)
+            $0.leading.equalTo(self.periodLabel.snp.trailing).offset(Metric.leadingMargin)
+            $0.bottom.equalTo(self.periodLabel.snp.bottom)
+        }
+    }
 }
