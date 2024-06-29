dbe541bdc82f0e7923b0d7f06b340bef07c43e56
hyeri00 <nexusring@gmail.com>
2024-06-29 17:38:57 +0900

    feat: 가장 최근 날짜의 객체 필터링

--- LivingFriend/ViewModel/HomeViewModel.swift
+++ LivingFriend/ViewModel/HomeViewModel.swift
@@ -13,6 +13,7 @@ class HomeViewModel {
     
     var fetchedObjects: [MyManageEntity] = []
     var filteredObjects: [MyManageEntity] = []
+    var recentObjects: [MyManageEntity] = []
     
     private var managedObjectContext: NSManagedObjectContext {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -20,7 +21,7 @@ class HomeViewModel {
     }
     
     // MARK: - 모든 데이터 가져오기
-
+    
     func fetchObjects(completion: @escaping () -> Void) {
         let fetchRequest: NSFetchRequest<MyManageEntity> = MyManageEntity.fetchRequest()
         
@@ -32,8 +33,28 @@ class HomeViewModel {
         }
     }
     
+    // MARK: - 가장 최근 날짜의 객체 필터링
+    
+    func filterMostRecentObjects(_ objects: [MyManageEntity]) {
+        var uniqueObjects = [String: MyManageEntity]()
+        
+        for object in objects {
+            guard let title = object.categoryTitle else { continue }
+            
+            if let existingObject = uniqueObjects[title] {
+                if let existingDate = existingObject.dateText, let newDate = object.dateText, newDate > existingDate {
+                    uniqueObjects[title] = object
+                }
+            } else {
+                uniqueObjects[title] = object
+            }
+        }
+        
+        self.recentObjects = Array(uniqueObjects.values)
+    }
+    
     // MARK: - 데이터 날짜별로 가져오기
-
+    
     func getFilteredObject(for date: String, completion: @escaping () -> Void) {
         let fetchRequest: NSFetchRequest<MyManageEntity> = MyManageEntity.fetchRequest()
         fetchRequest.predicate = NSPredicate(format: "dateText == %@", date)
@@ -47,7 +68,7 @@ class HomeViewModel {
     }
     
     // MARK: - 데이터 삭제하기
-
+    
     func deleteObject(at indexPath: IndexPath, completion: @escaping () -> Void) {
         let objectToDelete = fetchedObjects[indexPath.row]
         managedObjectContext.delete(objectToDelete)
@@ -62,7 +83,7 @@ class HomeViewModel {
     }
     
     // MARK: - 날짜 변환
-
+    
     func calculateDate(from dateText: String, withPeriod periodText: String) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
