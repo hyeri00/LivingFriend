c990e17376bd6637def2991433be5b6e28693bdc
hyeri00 <nexusring@gmail.com>
2024-06-30 22:16:04 +0900

    feat: ManageDetailView UI 추가

---
+++ LivingFriend/Presentation/Manage/Views/ManageDetailView.swift
@@ -0,0 +1,33 @@
+//
+//  ManageDetailView.swift
+//  LivingFriend
+//
+//  Created by 혜리 on 6/30/24.
+//
+
+import UIKit
+
+final class ManageDetailView: UIView {
+    
+    // MARK: - UI
+    
+    private let recentDateLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = Font.Typography.bold30
+        return label
+    }()
+    
+    private lazy var listTableView: UITableView = {
+        let view = UITableView()
+        view.rowHeight = 52
+        view.backgroundColor = .white
+        view.separatorStyle = .none
+        
+        view.delegate = self
+        view.dataSource = self
+        view.register(ManageDetailTableViewCell.self,
+                      forCellReuseIdentifier: "ManageDetailTableViewCell")
+        return view
+    }()
+}
