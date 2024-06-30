7b8d437fc92847f77978b0434e670550e9d3b960
hyeri00 <nexusring@gmail.com>
2024-06-30 22:07:41 +0900

    feat: bgColor 지정

--- LivingFriend/Presentation/Manage/Cells/ManageDetailTableViewCell.swift
+++ LivingFriend/Presentation/Manage/Cells/ManageDetailTableViewCell.swift
@@ -30,4 +30,23 @@ final class ManageDetailTableViewCell: UITableViewCell {
         button.setImage(IconImages.delete.image, for: .normal)
         return button
     }()
+    
+    // MARK: - Initialize
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        self.backgroundColor = .white
+    }
+    
 }
