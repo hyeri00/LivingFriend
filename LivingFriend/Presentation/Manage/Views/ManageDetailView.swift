9e594b98468352789e40218cbe8e53ac7da57ea3
hyeri00 <nexusring@gmail.com>
2024-07-01 21:47:16 +0900

    feat: bgColor 지정

--- LivingFriend/Presentation/Manage/Views/ManageDetailView.swift
+++ LivingFriend/Presentation/Manage/Views/ManageDetailView.swift
@@ -30,6 +30,24 @@ final class ManageDetailView: UIView {
                       forCellReuseIdentifier: "ManageDetailTableViewCell")
         return view
     }()
+    
+    // MARK: - Initialize
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        self.backgroundColor = .white
+    }
 }
 
 // MARK: - UITableViewDelegate, UITableViewDataSource
