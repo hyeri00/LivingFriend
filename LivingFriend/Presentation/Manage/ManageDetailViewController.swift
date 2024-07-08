605124f2662e386aa327dff50acc618f04323b79
hyeri00 <nexusring@gmail.com>
2024-07-08 20:37:06 +0900

    feat: navigationBar 생성

--- LivingFriend/Presentation/Manage/ManageDetailViewController.swift
+++ LivingFriend/Presentation/Manage/ManageDetailViewController.swift
@@ -37,6 +37,31 @@ final class ManageDetailViewController: UIViewController {
     
     private func configure() {
         self.view.backgroundColor = .white
+        
+        self.setNavigationBar()
+    }
+    
+    private func setNavigationBar() {
+        let image = IconImages.back.image?.withTintColor(.black, renderingMode: .alwaysOriginal)
+        
+        let titleLabel = UILabel()
+        titleLabel.text = TextManager.manage_detail_title
+        titleLabel.font = Font.Typography.bold16
+        titleLabel.textColor = .black
+        titleLabel.sizeToFit()
+        self.navigationItem.titleView = titleLabel
+        
+        self.navigationController?.navigationBar.barTintColor = .white
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: image, style: .done,
+            target: self, action: #selector(showPrevious)
+        )
+    }
+    
+    @objc
+    private func showPrevious() {
+        self.navigationController?.popViewController(animated: true)
     }
 }
 
