4c38800cf41b1e39fbc12bcdb9c9765b6d467d6f
hyeri00 <nexusring@gmail.com>
2024-08-15 17:30:20 +0900

    refactor: isHidden 제거

--- LivingFriend/Presentation/Manage/Cells/ManageDetailTableViewCell.swift
+++ LivingFriend/Presentation/Manage/Cells/ManageDetailTableViewCell.swift
@@ -40,7 +40,6 @@ final class ManageDetailTableViewCell: UITableViewCell {
     private let deleteButton: UIButton = {
         let button = UIButton()
         button.setImage(IconImages.delete.image, for: .normal)
-        button.isHidden = true // TODO: 기능 구현
         return button
     }()
     
