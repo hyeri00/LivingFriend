5fca85920ad6d382b2a06718494b8b0dcf99e49f
hyeri00 <nexusring@gmail.com>
2024-08-18 15:30:37 +0900

    feat: navigationBar 설정

--- LivingFriend/Presentation/Manage/ManageWholeViewController.swift
+++ LivingFriend/Presentation/Manage/ManageWholeViewController.swift
@@ -37,5 +37,30 @@ final class ManageWholeViewController: UIViewController {
     
     private func configure() {
         self.view.backgroundColor = .white
+        
+        self.setNavigationBar()
+    }
+    
+    private func setNavigationBar() {
+        let image = IconImages.back.image?.withTintColor(.black, renderingMode: .alwaysOriginal)
+        
+        let titleLabel = UILabel()
+        titleLabel.text = TextManager.manage_whole_title
+        titleLabel.font = Font.Typography.bold16
+        titleLabel.textColor = .black
+        titleLabel.sizeToFit()
+        self.navigationItem.titleView = titleLabel
+        
+        self.navigationController?.navigationBar.barTintColor = .white
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: image, style: .done,
+            target: self, action: #selector(showPrevious)
+        )
+    }
+    
+    @objc
+    private func showPrevious() {
+        self.navigationController?.popViewController(animated: true)
     }
 }
