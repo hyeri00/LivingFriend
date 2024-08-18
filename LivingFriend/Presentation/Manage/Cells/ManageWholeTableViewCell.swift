02274cfc1cb8e0a1abc2ac0d5a7e8aaffba1c660
hyeri00 <nexusring@gmail.com>
2024-08-18 14:54:39 +0900

    feat: configure

--- LivingFriend/Presentation/Manage/Cells/ManageWholeTableViewCell.swift
+++ LivingFriend/Presentation/Manage/Cells/ManageWholeTableViewCell.swift
@@ -31,4 +31,22 @@ final class ManageWholeTableViewCell: UITableViewCell {
         label.font = Font.Typography.air14
         return label
     }()
+    
+    // MARK: - Initialize
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Configure
+    
+    private func configure() {
+        
+    }
 }
