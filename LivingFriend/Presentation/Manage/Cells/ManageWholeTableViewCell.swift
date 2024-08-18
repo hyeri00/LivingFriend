4b8f64d61271b4d5d96be649a985e1f8b53321e7
hyeri00 <nexusring@gmail.com>
2024-08-18 14:50:47 +0900

    feat: label 추가

---
+++ LivingFriend/Presentation/Manage/Cells/ManageWholeTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ManageWholeTableViewCell.swift
+//  LivingFriend
+//
+//  Created by 혜리 on 8/18/24.
+//
+
+import UIKit
+
+final class ManageWholeTableViewCell: UITableViewCell {
+    
+    // MARK: - UI
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = Font.Typography.air14
+        return label
+    }()
+}
