b061c57d22b98376e59ee8ed1e98c27786736d4d
hyeri00 <nexusring@gmail.com>
2024-08-19 22:09:41 +0900

    feat: HomeCoordinator 작성

---
+++ LivingFriend/Coordinator/HomeCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  HomeCoordinator.swift
+//  LivingFriend
+//
+//  Created by 혜리 on 8/18/24.
+//
+
+import UIKit
+
+final class HomeCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let homeViewController = HomeViewController()
+        homeViewController.coordinator = self
+        self.navigationController.pushViewController(homeViewController, animated: false)
+    }
+    
+    func showCategory() {
+        // TODO: - CategoryCoordinator 연결
+    }
+}
