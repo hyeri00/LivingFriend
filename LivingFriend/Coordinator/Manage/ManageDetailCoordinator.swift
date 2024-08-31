fbe1a0bd83bc1ced9a42a4f88500d50f8dc84544
hyeri00 <nexusring@gmail.com>
2024-08-31 15:43:31 +0900

    feat: coordinator 작성

---
+++ LivingFriend/Coordinator/Manage/ManageDetailCoordinator.swift
@@ -0,0 +1,23 @@
+//
+//  ManageDetailCoordinator.swift
+//  LivingFriend
+//
+//  Created by 혜리 on 8/31/24.
+//
+
+import UIKit
+
+final class ManageDetailCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let manageDetailViewController = ManageDetailViewController()
+        manageDetailViewController.coordinator = self
+        self.navigationController.pushViewController(manageDetailViewController, animated: false)
+    }
+}
