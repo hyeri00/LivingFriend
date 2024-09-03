0825be67f2da9dc1f3e96b7d97202c34ac9ece8b
hyeri00 <nexusring@gmail.com>
2024-09-03 23:41:56 +0900

    feat: ManageWholeCoordinator 작성:

---
+++ LivingFriend/Coordinator/Manage/ManageWholeCoordinator.swift
@@ -0,0 +1,23 @@
+//
+//  ManageWholeCoordinator.swift
+//  LivingFriend
+//
+//  Created by 혜리 on 9/3/24.
+//
+
+import UIKit
+
+final class ManageWholeCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let manageWholeViewController = ManageWholeViewController()
+        manageWholeViewController.coordinator = self
+        self.navigationController.pushViewController(manageWholeViewController, animated: false)
+    }
+}
